a4c17d287a40e17b3c73789000792db77ae04ad9
Enzo Briones <enzo.briones@inacapmail.cl>
2024-05-04 11:59:26 -0400

    Update water level chart with dynamic data and loading indicator

--- lib/presentation/widgets/charts/water_level_chart.dart
+++ lib/presentation/widgets/charts/water_level_chart.dart
@@ -1,8 +1,26 @@
 import 'package:canalistas_laja_inacap/presentation/widgets/charts/chart_titles.dart';
+import 'package:canalistas_laja_inacap/services/api_service.dart';
 import 'package:flutter/material.dart';
 
-class WaterLevelChart extends StatelessWidget {
+class WaterLevelChart extends StatefulWidget {
   const WaterLevelChart({super.key});
+
+  @override
+  State<WaterLevelChart> createState() => _WaterLevelChartState();
+}
+
+class _WaterLevelChartState extends State<WaterLevelChart> {
+  dynamic thingSpeakData;
+
+  @override
+  void initState() {
+    super.initState();
+    getThingSpeakData().then((data) {
+      setState(() {
+        thingSpeakData = data;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,19 +34,43 @@ class WaterLevelChart extends StatelessWidget {
           children: [
             Column(children: [
               const ChartTitle(text: 'Altura del agua'),
-              const Placeholder(
-                fallbackHeight: 250,
+              Container(
+                height: 500,
+                child: thingSpeakData == null
+                    ? const Center(child: CircularProgressIndicator())
+                    : ListView.builder(
+                        itemCount: thingSpeakData['feeds'].length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title:
+                                Text(thingSpeakData['feeds'][index]['field1']),
+                            subtitle: Text(
+                                thingSpeakData['feeds'][index]['created_at']),
+                          );
+                        },
+                      ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Wrap(
                   spacing: 20,
                   children: [
-                    FilledButton.icon(onPressed: () {}, label: const Text('Horas'), icon: const Icon(Icons.hourglass_empty_outlined),),
-                    FilledButton.icon(onPressed: () {}, label: const Text('Días'), icon: const Icon(Icons.calendar_today_outlined),),
-                    FilledButton.icon(onPressed: () {}, label: const Text('Semanas'), icon: const Icon(Icons.calendar_month_outlined),),
+                    FilledButton.icon(
+                      onPressed: () {},
+                      label: const Text('Horas'),
+                      icon: const Icon(Icons.hourglass_empty_outlined),
+                    ),
+                    FilledButton.icon(
+                      onPressed: () {},
+                      label: const Text('Días'),
+                      icon: const Icon(Icons.calendar_today_outlined),
+                    ),
+                    FilledButton.icon(
+                      onPressed: () {},
+                      label: const Text('Semanas'),
+                      icon: const Icon(Icons.calendar_month_outlined),
+                    ),
                   ],
-
                 ),
               ),
             ]),
